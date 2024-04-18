7a2814172b6ad851724c8701b62b58dc83c00b50
terance-tucker <104857327+terance-tucker@users.noreply.github.com>
2024-04-18 12:32:20 -0700

    updating all files

--- contact.js
+++ contact.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Get references to the button and the contact div
 const formButton = document.querySelector('.form-button');
 const closeButton = document.querySelector('button');
@@ -15,22 +14,4 @@ closeButton.addEventListener('click', () => {
     // Move the contact div to the bottom of the page
     contactDiv.style.position = 'absolute';
     contactDiv.style.bottom = '-450px';
-=======
-// Get references to the button and the contact div
-const formButton = document.querySelector('.form-button');
-const closeButton = document.querySelector('button');
-const contactDiv = document.querySelector('.contact');
-
-// Add a click event listener to the button
-formButton.addEventListener('click', () => {
-    // Move the contact div to the bottom of the page
-    contactDiv.style.position = 'absolute';
-    contactDiv.style.bottom = '0px';
-});
-
-closeButton.addEventListener('click', () => {
-    // Move the contact div to the bottom of the page
-    contactDiv.style.position = 'absolute';
-    contactDiv.style.bottom = '-450px';
->>>>>>> 958830dafc2dd64630bda072ea6460a2345e2699
 });
--- parallax.js
+++ parallax.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 const el = document.querySelector("body");
 el.addEventListener("mousemove", (e) => {
   const x = -e.clientX / window.innerWidth;
   const y = -e.clientY / window.innerHeight;
   el.style.setProperty("--x", x * 10 + "px");
   el.style.setProperty("--y", y * 10 + "px");
-=======
-const el = document.querySelector("body");
-el.addEventListener("mousemove", (e) => {
-  const x = -e.clientX / window.innerWidth;
-  const y = -e.clientY / window.innerHeight;
-  el.style.setProperty("--x", x * 10 + "px");
-  el.style.setProperty("--y", y * 10 + "px");
->>>>>>> 958830dafc2dd64630bda072ea6460a2345e2699
 });
